37346e1a4e30bda0af78e53b26bfcbbed687082f
ilijaradojkovic <radojkovicika@gmail.com>
2022-04-06 01:14:12 +0300

    napravljene domenske klase

---
+++ src/main/java/Models/Main.java
@@ -0,0 +1,35 @@
+package Models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+		try {
+			URL url=new URL("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=pizza&number=1&offset=0&type=main%20course&rapidapi-key=f399f134d5mshac60582ce8ec350p185769jsnc4294929c64c");
+			HttpURLConnection  con=(HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			BufferedReader bf=new BufferedReader(new InputStreamReader(con.getInputStream()));
+			Gson g=new Gson();
+		JeloRequest jr=	g.fromJson(bf,JeloRequest.class);
+		System.out.println(jr.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
